1747fcd1985e4a455bcdce9aa3a904dd0a12b657
smurtee <muratees@yahoo.com>
2022-09-25 19:17:41 -0500

    engineer tests complete

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,16 @@
+const Engineer = require ("../lib/Engineer")
+test ("get the Engineer github",()=>{
+    const github = "bengithub"
+    const newEngineer = new Engineer ("Ben", "02", "ben@place.com", "bengithub")
+    expect (newEngineer.github).toBe(github)
+})
+test ("test getGitHub function",()=>{
+    
+    const newEngineer = new Engineer ("Ben", "02", "ben@place.com", "bengithub")
+    expect (newEngineer.getGitHub()).toBe("bengithub")
+})
+test ("test getRole function",()=>{
+    
+    const newRole = new Engineer ("Ben", "02","ben@place.com", "bengithub")
+    expect (newRole.getRole()).toBe("Engineer")
+})
